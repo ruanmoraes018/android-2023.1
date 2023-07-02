da1ac77f177dbdcf04bbe1b19d4e59d2bb176bd6
Lucazzza <lucas.souza5294@gmail.com>
2023-07-02 19:23:36 -0300

    Activitys

--- app/src/main/java/com/android/tokentravel/Form_Login.java
+++ app/src/main/java/com/android/tokentravel/Form_Login.java
@@ -3,13 +3,29 @@ package com.android.tokentravel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
 
 public class Form_Login extends AppCompatActivity {
 
+    private TextView text_tela_cadastro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
+        IniciarComponentes();
 
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Form_Login.this, FormCadastro.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void IniciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
     }
 }
