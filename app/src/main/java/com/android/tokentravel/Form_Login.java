514eb782bc7ee07992839275dcf00187ebbade9c
Ruan <ruanmoraes012@gmail.com>
2023-07-02 18:23:51 -0300

    Activitys

---
+++ app/src/main/java/com/android/tokentravel/Form_Login.java
@@ -0,0 +1,15 @@
+package com.android.tokentravel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Form_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_login);
+
+    }
+}
