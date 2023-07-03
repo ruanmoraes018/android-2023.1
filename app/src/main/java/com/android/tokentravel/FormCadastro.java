f7e89fc72bf0f3a65424b07b9ef701c3784c60c8
Lucazzza <lucas.souza5294@gmail.com>
2023-07-02 22:11:55 -0300

    Activitys

--- app/src/main/java/com/android/tokentravel/FormCadastro.java
+++ app/src/main/java/com/android/tokentravel/FormCadastro.java
@@ -1,8 +1,9 @@
 package com.android.tokentravel;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class FormCadastro extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class FormCadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_cadastro);
+
+        Spinner spinnerUserType = findViewById(R.id.spinner_user_type);
+
+        String[] userTypes = {"Passageiro", "Motorista"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, userTypes);
+        spinnerUserType.setAdapter(adapter);
+
+        String selectedUserType = spinnerUserType.getSelectedItem().toString();
     }
 }
