524d4c33f46b5c9b5a24fc6ed76c307beefa3955
Ruan <ruanmoraes012@gmail.com>
2023-07-08 07:22:09 -0300

    Activity + Troca do Logo do Aplicativo

--- app/src/main/java/com/android/tokentravel/MainActivity.java
+++ app/src/main/java/com/android/tokentravel/MainActivity.java
@@ -2,13 +2,24 @@ package com.android.tokentravel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-
+Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Form_Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
