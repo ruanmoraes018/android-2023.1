4728341007ac84a20b130ce59c25e1a7ba83221a
Lucazzza <lucas.souza5294@gmail.com>
2023-07-24 13:40:35 -0300

    Adicionado a camera ao menu_fragment!

--- app/src/main/java/com/android/tokentravel/Navigation_View.java
+++ app/src/main/java/com/android/tokentravel/Navigation_View.java
@@ -5,6 +5,17 @@ import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacem
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,16 +26,6 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -52,6 +53,7 @@ public class Navigation_View extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
     private NavigationView navigationView;
+    private ImageView btnUpdateLocation; // ImageView for updating location
     private Button botaoPesquisarRota;
     private Button btpesquisaRota;
     private Stack<Fragment> fragmentStack;
@@ -117,17 +119,17 @@ public class Navigation_View extends AppCompatActivity {
                     drawerLayout.closeDrawer(GravityCompat.START);
                     Toast.makeText(Navigation_View.this, "Meus Dados", Toast.LENGTH_SHORT);
                     fragmentR(new MenuFragment());
-                    botaoPesquisarRota.setVisibility(View.GONE); // Torna o botão visível
+                    botaoPesquisarRota.setVisibility(View.GONE);
                 } else if (itemId == R.id.historico) {
                     fragmentR(new HistoricoFragment());
                     drawerLayout.closeDrawer(GravityCompat.START);
                     Toast.makeText(Navigation_View.this, "Histórico", Toast.LENGTH_SHORT);
-                    botaoPesquisarRota.setVisibility(View.GONE); // Torna o botão visível
+                    botaoPesquisarRota.setVisibility(View.GONE);
                 } else if (itemId == R.id.suporte) {
                     fragmentR(new SuporteFragment());
                     drawerLayout.closeDrawer(GravityCompat.START);
                     Toast.makeText(Navigation_View.this, "Fale Conosco", Toast.LENGTH_SHORT);
-                    botaoPesquisarRota.setVisibility(View.GONE); // Torna o botão visível
+                    botaoPesquisarRota.setVisibility(View.GONE);
                 } else {
                     // Item de deslogar da aplicação.
                 }
@@ -136,11 +138,21 @@ public class Navigation_View extends AppCompatActivity {
             }
         });
 
+        btpesquisaRota = findViewById(R.id.botao_pesquisar_rota);
         btpesquisaRota.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Navigation_View.this, PesquisaRota.class);
                 startActivity(intent);
+            }
+        });
+
+        // Find and set click listener for the "Update Location" ImageView
+        btnUpdateLocation = findViewById(R.id.btn_update_location);
+        btnUpdateLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateLocation();
             }
         });
     }
@@ -164,10 +176,10 @@ public class Navigation_View extends AppCompatActivity {
             if (!fragmentStack.isEmpty()) {
                 getSupportFragmentManager().popBackStackImmediate(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
                 fragmentStack.clear();
-                botaoPesquisarRota.setVisibility(View.VISIBLE); // Torna o botão visível
+                botaoPesquisarRota.setVisibility(View.VISIBLE);
             } else {
                 super.onBackPressed();
-                botaoPesquisarRota.setVisibility(View.VISIBLE); // Torna o botão visível
+                botaoPesquisarRota.setVisibility(View.VISIBLE);
             }
         } else {
             super.onBackPressed();
@@ -194,12 +206,10 @@ public class Navigation_View extends AppCompatActivity {
     private void initLocationClient() {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
-        // Configurar a solicitação de localização
         LocationRequest locationRequest = LocationRequest.create();
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        locationRequest.setInterval(5000); // Intervalo de atualização de localização (5 segundos)
+        locationRequest.setInterval(5000);
 
-        // Criar o callback para obter a localização atual
         locationCallback = new LocationCallback() {
             @Override
             public void onLocationResult(LocationResult locationResult) {
@@ -208,35 +218,50 @@ public class Navigation_View extends AppCompatActivity {
                 }
 
                 for (Location location : locationResult.getLocations()) {
-                    // Verificar se a localização é válida
                     if (location != null) {
-                        // Remover o marcador anterior
-                        mapboxMap.getStyle().removeLayer(iconLayer);
-                        mapboxMap.getStyle().removeSource(iconGeoJsonSource);
-
-                        // Adicionar marcador para a localização atual
+                        // Update the user's location on the map
                         addMarker(location.getLatitude(), location.getLongitude());
-
-                        // Centralizar o mapa na localização atual
                         mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
                                 new LatLng(location.getLatitude(), location.getLongitude()), 15.0));
 
-                        // Parar de receber atualizações de localização após obter uma localização válida
+                        // Stop receiving location updates
                         fusedLocationClient.removeLocationUpdates(locationCallback);
+
+                        // Show the "Update Location" ImageView after getting the initial location
+                        btnUpdateLocation.setVisibility(View.VISIBLE);
                     }
                 }
             }
         };
 
-        // Iniciar atualizações de localização
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
+        // Start receiving location updates
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
         }
-        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+    }
+
+    private void updateLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            initLocationClient();
+
+            btnUpdateLocation.setVisibility(View.GONE);
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    1);
+        }
     }
 
     private void addMarker(double latitude, double longitude) {
         if (mapboxMap != null && mapboxMap.getStyle() != null) {
+            if (iconGeoJsonSource != null) {
+                mapboxMap.getStyle().removeLayer(iconLayer);
+                mapboxMap.getStyle().removeSource(iconGeoJsonSource.getId());
+            }
+
             iconGeoJsonSource = new GeoJsonSource("icon-source-id",
                     Feature.fromGeometry(Point.fromLngLat(longitude, latitude)));
             mapboxMap.getStyle().addSource(iconGeoJsonSource);
