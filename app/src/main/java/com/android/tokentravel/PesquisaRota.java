12b3e6d74b18d50190f7e39cfa4b7c39f82e4984
Lucazzza <lucas.souza5294@gmail.com>
2023-08-07 20:30:58 -0300

    Adicionado notificação na activity pesquisa_rota!

--- app/src/main/java/com/android/tokentravel/PesquisaRota.java
+++ app/src/main/java/com/android/tokentravel/PesquisaRota.java
@@ -1,8 +1,13 @@
 package com.android.tokentravel;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -39,5 +44,24 @@ public class PesquisaRota extends AppCompatActivity {
             }
         };
         spinnerUserType.setAdapter(adapter);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("channel1","notification", NotificationManager.IMPORTANCE_HIGH);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void notificationButton (View view){
+        final String CHANNEL_ID = "channel1";
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notifications)
+                .setContentTitle("Ola passageiro")
+                .setContentText("Viemos desejar boa viagem!");
+
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.notify(1, builder.build());
     }
 }
