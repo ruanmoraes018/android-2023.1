f9f018faca28c462b765e8ad6dfb4e3718f7ac49
Lucazzza <lucas.souza5294@gmail.com>
2023-08-23 15:48:51 -0300

    Add metodo de obter coordenadas + visualização de rota ponto a ponto

--- app/src/main/java/com/android/tokentravel/PesquisaRota.java
+++ app/src/main/java/com/android/tokentravel/PesquisaRota.java
@@ -6,21 +6,42 @@ import androidx.core.app.NotificationCompat;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import com.mapbox.api.geocoding.v5.GeocodingCriteria;
+import com.mapbox.api.geocoding.v5.MapboxGeocoding;
+import com.mapbox.api.geocoding.v5.models.CarmenFeature;
+import com.mapbox.api.geocoding.v5.models.GeocodingResponse;
+import com.mapbox.geojson.Point;
+
+import java.util.List;
 
 public class PesquisaRota extends AppCompatActivity {
+
+    private EditText editOrigem; // Adicione esta variável
+    private EditText editDestino; // Adicione esta variável
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pesquisa_rota);
+
+        editOrigem = findViewById(R.id.edit_origem); // Inicialize o EditText
+        editDestino = findViewById(R.id.edit_destino);
 
         Spinner spinnerUserType = findViewById(R.id.spinner_user_type);
 
@@ -63,5 +84,50 @@ public class PesquisaRota extends AppCompatActivity {
 
         NotificationManager notificationManager = getSystemService(NotificationManager.class);
         notificationManager.notify(1, builder.build());
+
+        //Metodo para pegar as coordenadas de origem e destino
+        String endereco = editOrigem.getText().toString();
+        String endereco2 = editDestino.getText().toString();
+
+        MapboxGeocoding mapboxGeocoding = MapboxGeocoding.builder()
+                .accessToken(getResources().getString(R.string.mapbox_access_token))
+                .query(endereco + " " + endereco2)
+                .geocodingTypes(GeocodingCriteria.TYPE_PLACE)
+                .build();
+
+        mapboxGeocoding.enqueueCall(new Callback<GeocodingResponse>() {
+            @Override
+            public void onResponse(Call<GeocodingResponse> call, Response<GeocodingResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    List<CarmenFeature> features = response.body().features();
+                    if (features.size() >= 2) {
+                        CarmenFeature featureOrigem = features.get(0);
+                        CarmenFeature featureDestino = features.get(1);
+
+                        Point coordenadasOrigem = featureOrigem.center();
+                        Point coordenadasDestino = featureDestino.center();
+
+                        double latitudeOrigem = coordenadasOrigem.latitude();
+                        double longitudeOrigem = coordenadasOrigem.longitude();
+
+                        double latitudeDestino = coordenadasDestino.latitude();
+                        double longitudeDestino = coordenadasDestino.longitude();
+
+                        // Dessa forma serão passadas para a próxima activity as coordenadas de origem e destino
+                        Intent intent = new Intent(PesquisaRota.this, RotaPontoaPonto.class);
+                        intent.putExtra("latitudeOrigem", latitudeOrigem);
+                        intent.putExtra("longitudeOrigem", longitudeOrigem);
+                        intent.putExtra("latitudeDestino", latitudeDestino);
+                        intent.putExtra("longitudeDestino", longitudeDestino);
+                        startActivity(intent);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GeocodingResponse> call, Throwable t) {
+               // Toast.makeText(PesquisaRota.this, "Não foi possível obter o ponto de origem, forneça manualmente!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/android/tokentravel/RotaPontoaPonto.java
@@ -0,0 +1,223 @@
+package com.android.tokentravel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.mapbox.api.directions.v5.DirectionsCriteria;
+import com.mapbox.api.directions.v5.MapboxDirections;
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.LineString;
+import com.mapbox.geojson.Point;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.geometry.LatLngBounds;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.style.layers.LineLayer;
+import com.mapbox.mapboxsdk.style.layers.Property;
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import com.mapbox.mapboxsdk.utils.BitmapUtils;
+
+import androidx.annotation.NonNull;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.mapbox.core.constants.Constants.PRECISION_6;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
+
+public class RotaPontoaPonto extends AppCompatActivity {
+
+    private MapView mapView;
+    private MapboxDirections client;
+    private Point origin;
+    private Point destination;
+    private double latitudeOrigem;
+    private double longitudeOrigem;
+    private double latitudeDestino;
+    private double longitudeDestino;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rota_pontoa_ponto);
+
+        // Recupere as coordenadas de origem e destino da Intent
+        latitudeOrigem = getIntent().getDoubleExtra("latitudeOrigem", 0.0);
+        longitudeOrigem = getIntent().getDoubleExtra("longitudeOrigem", 0.0);
+        latitudeDestino = getIntent().getDoubleExtra("latitudeDestino", 0.0);
+        longitudeDestino = getIntent().getDoubleExtra("longitudeDestino", 0.0);
+
+        // Inicializando o MapView
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+                        origin = Point.fromLngLat(longitudeOrigem, latitudeOrigem);
+                        destination = Point.fromLngLat(longitudeDestino, latitudeDestino);
+
+                        initSource(style);
+                        getRoute(mapboxMap, origin, destination);
+
+                        // Ajustando a camera para concentrar na rota
+                        LatLngBounds routeBounds = new LatLngBounds.Builder()
+                                .include(new LatLng(latitudeOrigem, longitudeOrigem))
+                                .include(new LatLng(latitudeDestino, longitudeDestino))
+                                .build();
+
+                        mapboxMap.easeCamera(CameraUpdateFactory.newLatLngBounds(routeBounds, 200), 2000);
+
+                        LineLayer routeLayer = new LineLayer("route-layer-id", "route-source-id");
+                        routeLayer.setProperties(
+                                PropertyFactory.lineCap(Property.LINE_CAP_ROUND),
+                                PropertyFactory.lineJoin(Property.LINE_JOIN_ROUND),
+                                PropertyFactory.lineWidth(5f),
+                                PropertyFactory.lineColor(Color.parseColor("#009688"))
+                        );
+                        style.addLayer(routeLayer);
+
+                        style.addImage("red-pin-icon-id", BitmapUtils.getBitmapFromDrawable(getResources().getDrawable(R.drawable.ic_inicio_map)));
+
+                        SymbolLayer originSymbolLayer = new SymbolLayer("origin-symbol-layer-id", "icon-source-id1")
+                                .withProperties(
+                                        iconImage("red-pin-icon-id"),
+                                        iconIgnorePlacement(true),
+                                        iconAllowOverlap(true),
+                                        iconOffset(new Float[]{0f, -9f})
+                                );
+                        style.addLayer(originSymbolLayer);
+
+                        style.addImage("blue-pin-icon-id", BitmapUtils.getBitmapFromDrawable(getResources().getDrawable(R.drawable.ic_destino_map)));
+
+                        SymbolLayer destinationSymbolLayer = new SymbolLayer("destination-symbol-layer-id", "icon-source-id2")
+                                .withProperties(
+                                        iconImage("blue-pin-icon-id"),
+                                        iconIgnorePlacement(true),
+                                        iconAllowOverlap(true),
+                                        iconOffset(new Float[]{0f, -9f})
+                                );
+                        style.addLayer(destinationSymbolLayer);
+
+                        GeoJsonSource iconGeoJsonSource1 = new GeoJsonSource("icon-source-id1", FeatureCollection.fromFeatures(new Feature[]{
+                                Feature.fromGeometry(Point.fromLngLat(longitudeOrigem, latitudeOrigem))
+                        }));
+                        style.addSource(iconGeoJsonSource1);
+
+                        GeoJsonSource iconGeoJsonSource2 = new GeoJsonSource("icon-source-id2", FeatureCollection.fromFeatures(new Feature[]{
+                                Feature.fromGeometry(Point.fromLngLat(longitudeDestino, latitudeDestino))
+                        }));
+                        style.addSource(iconGeoJsonSource2);
+                    }
+                });
+            }
+        });
+    }
+
+    private void initSource(@NonNull Style loadedMapStyle) {
+        loadedMapStyle.addSource(new GeoJsonSource("route-source-id"));
+    }
+
+    private void getRoute(MapboxMap mapboxMap, Point origin, Point destination) {
+        client = MapboxDirections.builder()
+                .origin(origin)
+                .destination(destination)
+                .overview(DirectionsCriteria.OVERVIEW_FULL)
+                .profile(DirectionsCriteria.PROFILE_DRIVING)
+                .accessToken(getString(R.string.mapbox_access_token))
+                .build();
+
+        client.enqueueCall(new Callback<DirectionsResponse>() {
+            @Override
+            public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                if (response.body() == null || response.body().routes().size() < 1) {
+                    Toast.makeText(RotaPontoaPonto.this, "Nenhuma rota encontrada", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Obtenha o percurso de direções
+                DirectionsRoute currentRoute = response.body().routes().get(0);
+
+                if (mapboxMap != null) {
+                    mapboxMap.getStyle(new Style.OnStyleLoaded() {
+                        @Override
+                        public void onStyleLoaded(@NonNull Style style) {
+                            GeoJsonSource source = style.getSourceAs("route-source-id");
+
+                            if (source != null) {
+                                source.setGeoJson(LineString.fromPolyline(currentRoute.geometry(), PRECISION_6));
+                            }
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onFailure(Call<DirectionsResponse> call, Throwable throwable) {
+                Toast.makeText(RotaPontoaPonto.this, "Erro: " + throwable.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Cancelar a solicitação da API de direções
+        if (client != null) {
+            client.cancelCall();
+        }
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+}
