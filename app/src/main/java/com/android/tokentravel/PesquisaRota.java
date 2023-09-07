c6c0f533e955983bfec2789e2960d2b8323a2bd6
Lucazzza <lucas.souza5294@gmail.com>
2023-09-07 14:50:03 -0300

    atualizações em pesquisar rota

--- app/src/main/java/com/android/tokentravel/PesquisaRota.java
+++ app/src/main/java/com/android/tokentravel/PesquisaRota.java
@@ -1,9 +1,5 @@
 package com.android.tokentravel;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-
 import android.annotation.SuppressLint;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -15,39 +11,59 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
+import android.widget.AutoCompleteTextView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import com.mapbox.api.geocoding.v5.GeocodingCriteria;
 import com.mapbox.api.geocoding.v5.MapboxGeocoding;
 import com.mapbox.api.geocoding.v5.models.CarmenContext;
 import com.mapbox.api.geocoding.v5.models.CarmenFeature;
 import com.mapbox.api.geocoding.v5.models.GeocodingResponse;
 import com.mapbox.geojson.Point;
-
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class PesquisaRota extends AppCompatActivity {
 
-    private EditText editOrigem;
-    private EditText editDestino;
+    private AutoCompleteTextView autoCompleteOrigem;
+    private AutoCompleteTextView autoCompleteDestino;
+    private ArrayAdapter<String> autoCompleteAdapterOrigem;
+    private ArrayAdapter<String> autoCompleteAdapterDestino;
+    private Set<String> sugestoesUnicas = new HashSet<>();
+    private static final long AUTOCOMPLETE_DELAY = 100;
+    private Handler handler = new Handler(Looper.getMainLooper());
+    private Runnable autocompleteRunnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pesquisa_rota);
 
-        editOrigem = findViewById(R.id.edit_origem);
-        editDestino = findViewById(R.id.edit_destino);
+        autoCompleteOrigem = findViewById(R.id.autoCompleteOrigem);
+        autoCompleteDestino = findViewById(R.id.autoCompleteDestino);
+
+        autoCompleteAdapterOrigem = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line);
+        autoCompleteAdapterDestino = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line);
+
+        autoCompleteOrigem.setAdapter(autoCompleteAdapterOrigem);
+        autoCompleteDestino.setAdapter(autoCompleteAdapterDestino);
 
         Spinner spinnerUserType = findViewById(R.id.spinner_user_type);
 
@@ -72,28 +88,28 @@ public class PesquisaRota extends AppCompatActivity {
         };
         spinnerUserType.setAdapter(adapter);
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            NotificationChannel channel = new NotificationChannel("channel1","notification", NotificationManager.IMPORTANCE_HIGH);
+        // Configurar canais de notificação
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("channel1", "notification", NotificationManager.IMPORTANCE_HIGH);
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
         }
 
-        editOrigem = findViewById(R.id.edit_origem);
-
+        // Obter a localização atual
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         Criteria criteria = new Criteria();
         @SuppressLint("MissingPermission") Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
 
+        // Obter informações de localização da Mapbox
         if (location != null) {
-
             MapboxGeocoding geocodingService = MapboxGeocoding.builder()
                     .accessToken(getResources().getString(R.string.mapbox_access_token))
                     .query(Point.fromLngLat(location.getLongitude(), location.getLatitude()))
                     .geocodingTypes(GeocodingCriteria.TYPE_ADDRESS)
+                    .languages("pt-BR")
                     .build();
 
             geocodingService.enqueueCall(new Callback<GeocodingResponse>() {
-
                 @Override
                 public void onResponse(Call<GeocodingResponse> call, Response<GeocodingResponse> response) {
                     if (response.body() != null && !response.body().features().isEmpty()) {
@@ -102,11 +118,10 @@ public class PesquisaRota extends AppCompatActivity {
                         String stateName = getStateNameFromFeature(feature);
                         String countryName = getCountryNameFromFeature(feature);
 
-                        String locationInfo = cityName + ", " + stateName + ", " + countryName;
-                        editOrigem.setText(locationInfo);
+                        String locationInfo = cityName + " " + stateName + ", " + countryName;
+                        autoCompleteOrigem.setText(locationInfo);
                     }
                 }
-
 
                 @Override
                 public void onFailure(Call<GeocodingResponse> call, Throwable throwable) {
@@ -114,11 +129,113 @@ public class PesquisaRota extends AppCompatActivity {
                 }
             });
         }
+
+        // Configurar o TextWatcher para as caixas de texto de origem e destino
+        autoCompleteOrigem.addTextChangedListener(new TextWatcher() {
+            private CharSequence beforeText;
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                beforeText = charSequence;
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String newText = editable.toString();
+                handler.removeCallbacks(autocompleteRunnable);
+                handler.postDelayed(() -> consultarSugestoes(autoCompleteOrigem, autoCompleteAdapterOrigem), AUTOCOMPLETE_DELAY);
+            }
+
+        });
+
+        autoCompleteDestino.addTextChangedListener(new TextWatcher() {
+            private CharSequence beforeText;
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                beforeText = charSequence;
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Não é necessário implementar isso
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String newText = editable.toString();
+                handler.removeCallbacks(autocompleteRunnable);
+                handler.postDelayed(() -> consultarSugestoes(autoCompleteDestino, autoCompleteAdapterDestino), AUTOCOMPLETE_DELAY);
+            }
+        });
     }
 
+    private void consultarSugestoes(AutoCompleteTextView autoCompleteTextView, ArrayAdapter<String> adapter) {
+        String textoDigitado = autoCompleteTextView.getText().toString().trim();
+
+        if (!textoDigitado.isEmpty()) {
+            MapboxGeocoding geocodingService = MapboxGeocoding.builder()
+                    .accessToken(getResources().getString(R.string.mapbox_access_token))
+                    .query(textoDigitado)
+                    .geocodingTypes(GeocodingCriteria.TYPE_POI)
+                    .languages("pt-BR")
+                    .country("BR")
+                    .build();
+
+            geocodingService.enqueueCall(new Callback<GeocodingResponse>() {
+                @Override
+                public void onResponse(Call<GeocodingResponse> call, Response<GeocodingResponse> response) {
+                    if (response.isSuccessful() && response.body() != null) {
+                        List<CarmenFeature> features = response.body().features();
+                        if (features.size() >= 2) {
+                            sugestoesUnicas.clear();
+
+                            for (CarmenFeature feature : features) {
+                                String cidade = "";
+                                String estado = "";
+                                String pais = "";
+
+                                for (CarmenContext context : feature.context()) {
+                                    String type = context.id();
+                                    String text = context.text();
+
+                                    if (type.startsWith("place")) {
+                                        cidade = text;
+                                    } else if (type.startsWith("region")) {
+                                        estado = text;
+                                    } else if (type.startsWith("country")) {
+                                        pais = text;
+                                    }
+                                }
+
+                                String sugestao = cidade + " " + estado + ", " + pais;
+                                sugestoesUnicas.add(sugestao);
+                            }
+
+                            adapter.clear();
+                            adapter.addAll(new ArrayList<>(sugestoesUnicas));
+                            adapter.notifyDataSetChanged();
+                        }
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<GeocodingResponse> call, Throwable t) {
+                    Toast.makeText(PesquisaRota.this, "Algo deu errado, verifique sua conexão!", Toast.LENGTH_LONG).show();
+                }
+            });
+        } else {
+            adapter.clear();
+            adapter.notifyDataSetChanged();
+        }
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
-    public void notificationButton (View view){
+    public void notificationButton(View view) {
         final String CHANNEL_ID = "channel1";
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
@@ -129,42 +246,62 @@ public class PesquisaRota extends AppCompatActivity {
         NotificationManager notificationManager = getSystemService(NotificationManager.class);
         notificationManager.notify(1, builder.build());
 
-        //Metodo para pegar as coordenadas de origem e destino
-        String endereco = editOrigem.getText().toString();
-        String endereco2 = editDestino.getText().toString();
+        String enderecoOrigem = autoCompleteOrigem.getText().toString();
+        String enderecoDestino = autoCompleteDestino.getText().toString();
 
-        MapboxGeocoding mapboxGeocoding = MapboxGeocoding.builder()
+        if(TextUtils.isEmpty(enderecoOrigem)){
+            Toast.makeText(this, "A origem é obrigatório.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(enderecoDestino)){
+            Toast.makeText(this, "O destino é obrigatório.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        MapboxGeocoding geocodingServiceOrigem = MapboxGeocoding.builder()
                 .accessToken(getResources().getString(R.string.mapbox_access_token))
-                .query(endereco + " " + endereco2)
+                .query(enderecoOrigem)
                 .geocodingTypes(GeocodingCriteria.TYPE_PLACE)
                 .build();
 
-        mapboxGeocoding.enqueueCall(new Callback<GeocodingResponse>() {
+        geocodingServiceOrigem.enqueueCall(new Callback<GeocodingResponse>() {
             @Override
             public void onResponse(Call<GeocodingResponse> call, Response<GeocodingResponse> response) {
                 if (response.isSuccessful() && response.body() != null) {
-                    List<CarmenFeature> features = response.body().features();
-                    if (features.size() >= 2) {
-                        CarmenFeature featureOrigem = features.get(0);
-                        CarmenFeature featureDestino = features.get(1);
+                    CarmenFeature featureOrigem = response.body().features().get(0);
+                    Point coordenadasOrigem = featureOrigem.center();
+                    double latitudeOrigem = coordenadasOrigem.latitude();
+                    double longitudeOrigem = coordenadasOrigem.longitude();
 
-                        Point coordenadasOrigem = featureOrigem.center();
-                        Point coordenadasDestino = featureDestino.center();
+                    MapboxGeocoding geocodingServiceDestino = MapboxGeocoding.builder()
+                            .accessToken(getResources().getString(R.string.mapbox_access_token))
+                            .query(enderecoDestino)
+                            .geocodingTypes(GeocodingCriteria.TYPE_PLACE)
+                            .build();
 
-                        double latitudeOrigem = coordenadasOrigem.latitude();
-                        double longitudeOrigem = coordenadasOrigem.longitude();
+                    geocodingServiceDestino.enqueueCall(new Callback<GeocodingResponse>() {
+                        @Override
+                        public void onResponse(Call<GeocodingResponse> call, Response<GeocodingResponse> response) {
+                            if (response.isSuccessful() && response.body() != null) {
+                                CarmenFeature featureDestino = response.body().features().get(0);
+                                Point coordenadasDestino = featureDestino.center();
+                                double latitudeDestino = coordenadasDestino.latitude();
+                                double longitudeDestino = coordenadasDestino.longitude();
 
-                        double latitudeDestino = coordenadasDestino.latitude();
-                        double longitudeDestino = coordenadasDestino.longitude();
+                                Intent intent = new Intent(PesquisaRota.this, RotaPontoaPonto.class);
+                                intent.putExtra("latitudeOrigem", latitudeOrigem);
+                                intent.putExtra("longitudeOrigem", longitudeOrigem);
+                                intent.putExtra("latitudeDestino", latitudeDestino);
+                                intent.putExtra("longitudeDestino", longitudeDestino);
+                                startActivity(intent);
+                            }
+                        }
 
-                        // Dessa forma serão passadas para a próxima activity as coordenadas de origem e destino
-                        Intent intent = new Intent(PesquisaRota.this, RotaPontoaPonto.class);
-                        intent.putExtra("latitudeOrigem", latitudeOrigem);
-                        intent.putExtra("longitudeOrigem", longitudeOrigem);
-                        intent.putExtra("latitudeDestino", latitudeDestino);
-                        intent.putExtra("longitudeDestino", longitudeDestino);
-                        startActivity(intent);
-                    }
+                        @Override
+                        public void onFailure(Call<GeocodingResponse> call, Throwable t) {
+                            Toast.makeText(PesquisaRota.this, "Algo deu errado, verifique sua conexão!", Toast.LENGTH_LONG).show();
+                        }
+                    });
                 }
             }
 
