394192ec0ec9823dd29fd15ebdc8414e7bb541cf
Lucazzza <lucas.souza5294@gmail.com>
2023-09-30 21:37:05 -0300

    Perfil de apresentação do motorista add

---
+++ app/src/main/java/com/android/tokentravel/Perfil_Apresent_RotaProf.java
@@ -0,0 +1,14 @@
+package com.android.tokentravel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Perfil_Apresent_RotaProf extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_apresent_rota_prof);
+    }
+}
